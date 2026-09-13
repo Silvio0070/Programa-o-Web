0eba6540563c4327baf074a8614c40384e6d0fdb
Silvio0070 <catce.2025111isinf0070@aluno.ifpi.edu.br>
2026-09-13 10:37:18 -0300

    Update exercicio8.php

--- webbbb/Exercicios-Web-Professor-Luciano/404B 1.3 03.09 exercicios + qualitativo/silvio/php/php3/exercicio8.php
+++ webbbb/Exercicios-Web-Professor-Luciano/404B 1.3 03.09 exercicios + qualitativo/silvio/php/php3/exercicio8.php
@@ -4,8 +4,8 @@ $mais50 = 0;
 $media1020 = 0;
 $altura1020 = 0;
 $pessoasmenos40 = 0;
-$qp= readline("");
-while( $i <=25){
+$qp= readline("Insira a quantidade de pessoas: ");
+while( $i <=$qp){
     echo "\nPessoa $i: \n";
     $idade = readline("idade: \n" );
     $altura = readline("Altura: \n");
@@ -15,7 +15,7 @@ while( $i <=25){
     };
     if( $idade < 20 && $idade > 10){
         $media1020= $media1020+1;
-        $altura1020= $altura1020= $altura;
+        $altura1020= $altura1020+ $altura;
     }
     if ($peso <40){
         $pessoasmenos40 = $pessoasmenos40 + 1;
@@ -23,6 +23,7 @@ while( $i <=25){
     echo "Pessoa $i\n pessoas com menos de 40 kgs: $pessoasmenos40 \n pessoas com altura entre 10 e 20: $altura1020 \n pessoa mais 50: $mais50";
 	$i++;
 }
-$ma1020 = $media1020/$qp;
-echo "\n\n\n Resultados: \n Pessoas com idade superior a 50 anos: $mais50 \n media das alturas entre 1 e 20 anos: $ma1020 \n percentual de pessoas com menos de 40 kgs: "
+$ma1020 = $altura1020/$media1020;
+$per = $pessoasmenos40/($i -1);
+echo "\n\n\n Resultados: \n Pessoas com idade superior a 50 anos: $mais50 \n media das alturas entre 1 e 20 anos: $ma1020 \n percentual de pessoas com menos de 40 kgs: é $per"
 ?>
