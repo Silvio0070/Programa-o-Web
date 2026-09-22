9fd6301e673422ff083fa9fd792a20de7d1bac31
Silvio0070 <catce.2025111isinf0070@aluno.ifpi.edu.br>
2026-09-22 11:55:08 -0300

    Add files via upload

---
+++ webbbb/Exercicios-Web-Professor-Luciano/404B 1.3 03.09 exercicios + qualitativo/silvio/php/php5/exercicio1.php
@@ -0,0 +1,10 @@
+<?php
+$usuario = [
+    "nome" => "Silvio",
+    "Idade" => 17,
+    "email" => "email@dominio.com",
+    "cidade" => " Teresina"
+];
+
+echo  "O usuário " . $usuario['nome'] . " tem " . $usuario['Idade'] . " " . "e mora em " . $usuario['cidade'];
+?>
---
+++ webbbb/Exercicios-Web-Professor-Luciano/404B 1.3 03.09 exercicios + qualitativo/silvio/php/php5/exercicio3.php
@@ -0,0 +1,24 @@
+<?php
+$tudo = [
+    [
+    "nome" => "agua",
+    "preço" => 2.90,
+    "estoque" => 500
+    ],
+    [
+    "nome" => "cafe",
+    "preço" => 27.50,
+    "estoque" => 98
+    ],
+    [
+    "nome" => "sal",
+    "preço" => 1.80,
+    "estoque" => 200
+    ],
+];
+
+foreach($tudo as $prod => $val){
+    echo "";
+}
+
+?>
