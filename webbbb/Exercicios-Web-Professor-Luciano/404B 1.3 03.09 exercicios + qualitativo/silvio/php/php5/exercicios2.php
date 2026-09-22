5b0294c463ccad1129df5763bcc4af5e77fb3194
Silvio0070 <catce.2025111isinf0070@aluno.ifpi.edu.br>
2026-09-22 11:56:40 -0300

    Update exercicios2.php

---
+++ webbbb/Exercicios-Web-Professor-Luciano/404B 1.3 03.09 exercicios + qualitativo/silvio/php/php5/exercicios2.php
@@ -0,0 +1,13 @@
+<?php
+$turma = [
+    "aluno1" => 9,
+    "aluno2" => 4,
+    "aluno3" => 9,
+    "aluno4" => 4
+];
+
+foreach($turma as $aluno => $nota){
+    echo "O aluno $aluno tirou nota $nota \n";
+}
+
+?>
