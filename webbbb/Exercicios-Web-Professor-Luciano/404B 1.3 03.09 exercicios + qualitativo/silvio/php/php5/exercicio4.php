943accffd746f55b0e498d365e5660d9a8ec78a2
Silvio0070 <catce.2025111isinf0070@aluno.ifpi.edu.br>
2026-09-24 06:13:27 -0300

    Criar exercicio4.php

---
+++ webbbb/Exercicios-Web-Professor-Luciano/404B 1.3 03.09 exercicios + qualitativo/silvio/php/php5/exercicio4.php
@@ -0,0 +1,12 @@
+<?php
+$notasMaterias = [
+    'Matemática' => 8.0,
+    'Português' => 9.0,
+    'História' => 7.5,
+    'Geografia' => 8.5
+];
+
+$somaTotal = array_sum($notasMaterias);
+
+echo "A soma total das notas é: $somaTotal";
+?>
