61631d2724b59d5be27aa389e5da975bca5cd03a
Silvio0070 <catce.2025111isinf0070@aluno.ifpi.edu.br>
2026-09-24 07:09:53 -0300

    Atualizar o exercício6.php

--- webbbb/Exercicios-Web-Professor-Luciano/404B 1.3 03.09 exercicios + qualitativo/silvio/php/php5/exercício6.php
+++ webbbb/Exercicios-Web-Professor-Luciano/404B 1.3 03.09 exercicios + qualitativo/silvio/php/php5/exercício6.php
@@ -1,8 +1,8 @@
 <?php
 $contatos = [
-    ['nome' => 'João', 'telefone' => '11999998888', 'email' => 'joao@email.com'],
-    ['nome' => 'Maria', 'telefone' => '21988887777', 'email' => 'maria@email.com'],
-    ['nome' => 'Pedro', 'telefone' => '31977776666', 'email' => 'pedro@email.com']
+    ['nome' => 'João', 'telefone' => '8888888', 'email' => 'joao@email.com'],
+    ['nome' => 'Maria', 'telefone' => '77777777', 'email' => 'maria@email.com'],
+    ['nome' => 'Silvio', 'telefone' => '666666', 'email' => 'silvio@email.com']
 ];
 
 $totalContatos = count($contatos);
