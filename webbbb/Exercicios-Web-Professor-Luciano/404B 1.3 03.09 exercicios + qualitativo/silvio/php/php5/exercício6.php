b0f2258b3f16388ef3ceceb427beac02109c0745
Silvio0070 <catce.2025111isinf0070@aluno.ifpi.edu.br>
2026-09-24 06:14:27 -0300

    Criar exercício6.php

---
+++ webbbb/Exercicios-Web-Professor-Luciano/404B 1.3 03.09 exercicios + qualitativo/silvio/php/php5/exercício6.php
@@ -0,0 +1,11 @@
+<?php
+$contatos = [
+    ['nome' => 'João', 'telefone' => '11999998888', 'email' => 'joao@email.com'],
+    ['nome' => 'Maria', 'telefone' => '21988887777', 'email' => 'maria@email.com'],
+    ['nome' => 'Pedro', 'telefone' => '31977776666', 'email' => 'pedro@email.com']
+];
+
+$totalContatos = count($contatos);
+
+echo "Total de contatos cadastrados: $totalContatos";
+?>
